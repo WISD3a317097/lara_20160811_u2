cc350b2e1c9829d2dac5dc5019fb687fce181c73
s3a317097 <s3a317097@users.noreply.github.com>
2017-10-23 23:39:49 +0800

    設定 Route 接收參數-1

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,13 +10,16 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
+Route::get('/',function(){
+    return'welcome';
+});
 /*Route::get('/', function () {
     return view('welcome');
-});*/
-/*Route::get('/',function(){
-    return'welcome';
-});*/
+});
+
 Route::get('/',function(){
     return redirect('welcome');
+});*/
+Route::get('hello/{name}',function($name){
+  return'Hello,'.$name;
 });
